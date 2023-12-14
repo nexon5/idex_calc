de9adf39059243eda34a9fcbea953ec6a490863a
Dmitry Lychahin <dm.lychahin@gmail.com>
2023-12-14 17:21:57 +0200

    beta v0.2

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const height = document.getElementById('height');
 const px = document.getElementById('px');
 const btn = document.getElementById('calc');
 const factor = document.getElementById('factor');
+const btnCopy = document.getElementById('copy');
+const btnAuto = document.getElementById('auto');
+const info = document.getElementById('info-area');
 
 width.value = window.innerWidth;
 height.value = window.innerHeight;
+
+
 
 btn.onclick = (e) =>{
 	if(px.value !== ''){
@@ -15,3 +20,28 @@ btn.onclick = (e) =>{
 	}
 	
 }
+
+btnAuto.addEventListener('click', (e) =>{
+	width.value = window.innerWidth;
+	height.value = window.innerHeight;
+});
+
+btnCopy.onclick = (e) =>{
+	if(factor.value !== ''){
+	navigator.clipboard.writeText(`calc($index * ${factor.value})`)
+		.then(()=> {
+			btnCopy.innerText = 'Copied';
+			info.innerText = `Copied: calc($index * ${factor.value})`;
+			info.style.visibility='visible';
+			setTimeout(()=>{
+				btnCopy.innerText = 'Copy';
+				info.style.visibility='hidden';
+			},2000)
+		})
+		.catch((err)=>console.error(err));
+	}else{
+
+	}
+}
+
+	// 80a4ed #4D5382/#4D5382/#4D5382
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	purge: [
+	  './src/**/*.html',
+	],
+	darkMode: false, // или 'media' или 'class'
+	theme: {
+	  extend: {},
+	},
+	variants: {
+	  extend: {},
+	},
+	plugins: [
+	  // Добавьте здесь дополнительные плагины Tailwind CSS
+	],
+  }
+  
